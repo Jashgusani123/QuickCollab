fbf1c82ef4b6aaa6f4304b6ab0d25f361672b3dd
Jash Gusani <jashgusani78@gmail.com>
2025-12-22 19:32:57 +0530

    [chore]: remove thing & add envs

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend base URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // your backend base URL
   withCredentials: true, // allows cookies (needed for auth)
 });
 
